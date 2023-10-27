410d89fbe6606613b3d2beaa17a60d5c368f20a1
Michael <92550503+Voodoo-Dolls@users.noreply.github.com>
2023-10-27 00:15:10 -0600

    Added Arrows to Menu components
    -added arrow icon
    -created transitions between active states of dropdown

--- KF2-Bible-Web/src/components/Menu/Dropdown.jsx
+++ KF2-Bible-Web/src/components/Menu/Dropdown.jsx
@@ -1,17 +1,24 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
-const Dropdown = ({title, classNames, list}) => {
+const Dropdown = ({ title, classNames, list }) => {
   const [active, setActive] = useState(false);
 
-  const handleClick= () =>{setActive(!active)}
+  const handleClick = () => {
+    setActive(!active);
+  };
   return (
     <li className={`dropdown ${active ? "active" : ""}`} onClick={handleClick}>
-        <a href="#!">{title}</a>
-        <ul className={`dropdown-content ${classNames}`}>
-            {list}
-        </ul>
+      <a href="#!">
+        {title}{" "}
+        <img
+          src="../src/assets/img/menu/up-arrow.svg"
+          alt=""
+          className="caret"
+        />
+      </a>
+      <ul className={`dropdown-content ${classNames}`}>{list}</ul>
     </li>
-  )
-}
+  );
+};
 
-export default Dropdown
+export default Dropdown;
